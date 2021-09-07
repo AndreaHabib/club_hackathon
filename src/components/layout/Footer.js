0d65f0e26dd9454b6889a93e0e63eb5d54c8e085
prince-ao <princead@tutanota.com>
2021-09-07 14:00:32 -0400

    fix README, add dynamic year to footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,10 +3,11 @@ import "../styles/Footer.css";
 
 export default class Footer extends Component {
   render() {
+    let year = new Date().getFullYear();
     return (
       <div>
         <footer id="footer">
-          <p>&copy; Copyright 2021 HACK-A-PROJECT</p>
+          <p>&copy; Copyright {year} HACK-A-PROJECT</p>
         </footer>
       </div>
     );
