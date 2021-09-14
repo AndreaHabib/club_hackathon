9816158903900e854081a9fd6f7428b24eaeff8f
AndreaHabib <andrea.atef@yahoo.com>
2021-09-14 12:16:21 -0400

    FEAT: added react.js workshop, redesigned events

--- src/components/Events.js
+++ src/components/Events.js
@@ -8,29 +8,65 @@ export default function Events() {
         <strong>Events</strong>
       </h1>
       <div className="events">
-        <span className="event">
+        <div className="event">
           <img
-            id="hackcsi"
-            alt="hackcsi"
+            className="event_img"
             src="https://res.cloudinary.com/andreahabib/image/upload/v1627328388/Untitled_design_psaoo7.png"
+            alt="hackcsi"
           />
-
           <div className="info">
             <p className="title">HackCSI</p>
             <p className="description">
               A chill hackathon where CSI students can work togther on a
               project!
             </p>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              <i class="far fa-calendar-alt fa-1x"></i>
+              <p>Sept 3, 2021</p>
+            </div>
+            <div>
+              <span className="chip">Online</span>
+              <span className="chip">Hackathon</span>
+            </div>
             <a
               className="moreinfo"
+              rel="noreferrer noopener"
+              target="_blank"
+              href="https://hackcsi.devpost.com/project-gallery"
+              disabled
+            >
+              View The Winners
+            </a>
+          </div>
+        </div>
+        <div className="event">
+          <img
+            className="event_img"
+            src="https://res.cloudinary.com/andreahabib/image/upload/v1631628427/Introduction_to_unpho6.gif"
+            alt="hackcsi"
+          />
+          <div className="info">
+            <p className="title">React Workshop</p>
+            <p className="description">A workshop on React.js</p>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              <i class="far fa-calendar-alt fa-1x"></i>
+              <p>Sept 17, 2021</p>
+            </div>
+            <div>
+              <span className="chip">Online</span>
+              <span className="chip">Workshop</span>
+            </div>
+            <a
+              className="disabled"
               rel="noreferrer"
               target="_blank"
               href="https://hackcsi.netlify.app/"
+              disabled
             >
-              More Info
+              Zoom Link will be provided
             </a>
           </div>
-        </span>
+        </div>
       </div>
     </div>
   );
